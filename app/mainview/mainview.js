615170c972955a2452fc5d7b58d0e6692d6cabbf
Naguib <nick@perceptivemind.com.au>
2017-12-09 18:44:37 +1100

    Adjusted default files to my pereferences

---
+++ app/mainview/mainview.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.mainview', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/main', {
+    templateUrl: 'mainview/mainview.html',
+    controller: 'mainviewCtrl'
+  });
+}])
+
+.controller('mainviewCtrl', [function() {
+
+}]);
