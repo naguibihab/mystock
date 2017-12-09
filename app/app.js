9c3c68c59fbcf50be3abaab5bea4bccb1e057b97
Naguib <nick@perceptivemind.com.au>
2017-12-09 18:53:45 +1100

    Configured firebase

--- app/app.js
+++ app/app.js
@@ -4,10 +4,23 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.mainview',
-  'myApp.version'
+  'myApp.version',
+
+  'firebase'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/main'});
+
+  // Initialise Firebase
+  var config = {
+    apiKey: "AIzaSyCrjNulatMucABMGQvAc5bg8D35GU-eO3o",
+    authDomain: "mystock-34b49.firebaseapp.com",
+    databaseURL: "https://mystock-34b49.firebaseio.com",
+    projectId: "mystock-34b49",
+    storageBucket: "",
+    messagingSenderId: "925777963088"
+  };
+  firebase.initializeApp(config);
 }]);
