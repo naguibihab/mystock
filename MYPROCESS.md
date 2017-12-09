c433b65ff21f9cb4902b86e5f1244b2c225d3b9d
Naguib <nick@perceptivemind.com.au>
2017-12-09 18:53:45 +1100

    Using Environment variables

--- MYPROCESS.md
+++ MYPROCESS.md
@@ -9,3 +9,7 @@ I choose Firebase due to its ease and the ability to host on it.
 ### Decision 2
 My Second decision was to use Angular seed or simply go with a Pure Angular application, Angular Seed seems like an overkill given that I don't need any routing but I decided to go with it anyway as it will be the quicker option and having the option to do easy routing later on might come in handy for a future version of this project.
 
+
+# Code
+### Decision 3
+Because I'm hosting on github, I have to be careful with the kind of data that I put up there, I don't mind putting up my firebase config access but if I'll be using third partys' APIs such as Alpha Vintage, they might not appreciate having one of their keys public, so I've implemented a way to have environment variables that wouldn't be committed on git by following this article: https://www.jvandemo.com/how-to-configure-your-angularjs-application-using-environment-variables/
--- app/app.js
+++ app/app.js
@@ -1,13 +1,33 @@
 'use strict';
 
+// Import environment variables
+var env = {};
+if(window){
+	Object.assign(env, window.__env);
+}
+
+function disableLogging($logProvider, __env){  
+  $logProvider.debugEnabled(__env.enableDebug);
+}
+
+// Inject dependencies
+disableLogging.$inject = ['$logProvider', '__env'];
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.mainview',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+	'ngRoute',
+	'myApp.mainview',
+	'myApp.version',
 
-  $routeProvider.otherwise({redirectTo: '/main'});
-}]);
+	'firebase'
+])
+.constant('__env', env)
+.config(disableLogging)
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+	$locationProvider.hashPrefix('!');
+
+	$routeProvider.otherwise({redirectTo: '/main'});
+
+	// Initialise Firebase
+	firebase.initializeApp(__env.firebase);
+}]); 
